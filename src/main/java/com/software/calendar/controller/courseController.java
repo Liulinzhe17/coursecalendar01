0e3e32876038ef3e7e4749652d172764a5e3837f
wjzzljt <616125215@qq.com>
2017-12-23 16:53:08 +0800

    course 接口

--- src/main/java/com/software/calendar/controller/courseController.java
+++ src/main/java/com/software/calendar/controller/courseController.java
@@ -3,16 +3,16 @@ package com.software.calendar.controller;
 import com.software.calendar.bean.*;
 import com.software.calendar.repository.courseRepository;
 import com.software.calendar.repository.coursedataRepository;
+import com.software.calendar.repository.studentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 @RestController
-@RequestMapping(value="/course")
+@RequestMapping(value="/courses")
 public class courseController {
 
     @Autowired
@@ -21,9 +21,12 @@ public class courseController {
     @Autowired
     coursedataRepository coursedataRepo;
 
+    @Autowired
+    studentRepository studentRepo;
+
     @RequestMapping(value="/findweek",method = RequestMethod.POST)
 
-    public String Findweek(String id){//这里的参数名要和ajax传过来的对应
+    public String findweek(String id){//这里的参数名要和ajax传过来的对应
         course c = courseRepo.findByCourseId(id);
         int start = c.getCourseWeekstart();
         int end = c.getCourseWeekend();
@@ -31,26 +34,26 @@ public class courseController {
     }
 
     @RequestMapping(value="/findcourse",method = RequestMethod.POST)
-    public course Findhea(String cid){
+    public course findhea(String cid){
         course c = courseRepo.findByCourseId(cid);
         return c;
     }
-    @RequestMapping(value="/finddata",method=RequestMethod.POST)
-    public coursedata Finddata(String cid,String week){
+    @RequestMapping(value="/coursedata",method=RequestMethod.POST)
+    public coursedata coursedatas(String cid,String week){
         int we = Integer.parseInt(week);
         coursedata cd = coursedataRepo.findByCourseIdAndCourseWeektime(cid,we);
         return cd;
     }
 
-    @RequestMapping(value="/findteacher",method=RequestMethod.POST)
-    public Set<teacher> Findteacher(String cid){
+    @RequestMapping(value="/teacherinfo",method=RequestMethod.POST)
+    public Set<teacher> teacherinfo(String cid){
         course c = courseRepo.findByCourseId(cid);
         return c.getTeachers();
     }
 
 
     @RequestMapping(value="/updatec_outline",method=RequestMethod.POST)
-    public int Updatec_outline(String cid,String con,String week){
+    public int updatec_outline(String cid,String con,String week){
         int we = Integer.parseInt(week);
         coursedata cd = coursedataRepo.findByCourseIdAndCourseWeektime(cid,we);
         System.out.println(cd.toString());
@@ -60,7 +63,7 @@ public class courseController {
     }
 
     @RequestMapping(value="/updatec_homework",method=RequestMethod.POST)
-    public int Updatec_homework(String cid,String con,String week){
+    public int updatec_homework(String cid,String con,String week){
         int we = Integer.parseInt(week);
         coursedata cd = coursedataRepo.findByCourseIdAndCourseWeektime(cid,we);
         cd.setCourseHomework(con);
@@ -69,9 +72,52 @@ public class courseController {
     }
 
     @RequestMapping(value="/getallcourse",method = RequestMethod.POST)
-    public course Getallcourse(String stuid){
-        course c = courseRepo.findByCourseId(stuid);
-        return c;
+    public Set<course> getallcourse(String stuserid){
+        student stu = studentRepo.findByStuUserid(stuserid);
+        return stu.getCourses();
     }
-
+    @RequestMapping(value="/stuUserid/weektime",method = RequestMethod.GET)//本周课程
+    public Set<course> getcourses(String stuserid,String weektime){
+        int we = Integer.parseInt(weektime);
+        student stu = studentRepo.findByStuUserid(stuserid);
+        Set<course> courses = stu.getCourses();
+        Iterator<course> it = courses.iterator();
+        for(int index=0;index<courses.size();index++){
+            course nextc =  it.next();
+            int weekstart = nextc.getCourseWeekstart();
+            int weekend = nextc.getCourseWeekend();
+            if(we<weekstart||we>weekend){
+                it.remove();
+                index--;
+            }
+        }
+        return courses;
+    }
+    @RequestMapping(value = "/courseId",method = RequestMethod.GET)//获取单个详情
+    public course getcourse(String courseId){
+        return courseRepo.findByCourseId(courseId);
+    }
+    @RequestMapping(method = RequestMethod.POST)//新建课程
+    public int addcourse(@RequestBody course c){
+        System.out.println(c.toString());
+        courseRepo.saveAndFlush(c);
+        return 1;
+    }
+    @RequestMapping(value = "/{courseId}",method = RequestMethod.PUT)//更新课程
+    public int updatecourse(@PathVariable("courseId")String courseId,@RequestBody course c){
+        courseRepo.saveAndFlush(c);
+        return 1;
+    }
+    @RequestMapping(value = "/courseId",method = RequestMethod.DELETE)//删除课程
+    public int deletecourse(String courseId){
+        courseRepo.delete(courseId);
+        return 1;
+    }
+    @RequestMapping(method = RequestMethod.DELETE)//批量删除课程
+    public int deletecourses(@RequestBody String[] courseIdArray){
+        for (String courseId : courseIdArray){
+            studentRepo.delete(courseId);
+        }
+        return 1;
+    }
 }
